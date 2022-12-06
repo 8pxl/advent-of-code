1bf376044754685c9fc87ec1b6d96fa397b5934e
8pxl <keijayhuang@gmail.com>
2022-12-06 10:36:41 -0800

    day 3!

---
+++ 2022/day3/1.py
@@ -0,0 +1,17 @@
+inp = open("usaco/advent-of-code/2022/day3/input")
+
+priority = 0
+for line in inp:
+    mid = int(len(line.strip())/2)
+    first = ''.join(set((line[:mid])))
+    last = ''.join(set((line[mid:])))
+
+    for char in first:
+        if char in last:
+            uni = ord(char)
+            if uni <= 90:
+                uni -= 38
+            else:
+                uni -= 96
+            priority += uni
+print(priority)
---
+++ 2022/day3/2.py
@@ -0,0 +1,16 @@
+inp = open("usaco/advent-of-code/2022/day3/input")
+
+priority = 0   
+lines = [''.join(set(line.strip())) for line in inp.readlines()]
+triples = [lines[i:i+3] for i in range(0,len(lines),3)]
+for triple in triples:
+    for char in triple[0]:
+        if char in triple[1] and char in triple[2]:
+            print(char)
+            uni = ord(char)
+            if uni <= 90:
+                uni -= 38
+            else:
+                uni -= 96
+            priority += uni
+print(priority) 
