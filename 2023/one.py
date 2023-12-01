4ab8f46aa51fc3f472a8bdc8ee78d32e6bd76188
8pxl <56054380+8pxl@users.noreply.github.com>
2023-12-01 09:03:01 -0800

    Update one.py

--- 2023/one.py
+++ 2023/one.py
@@ -1,7 +1,6 @@
-input = open("1.in")
-pairs = [ ('one', 'on1ne'), ('two', 'tw2wo'), ('three', 'thr3ree'), ('four', 'fo4ur'), ('five', 'fi5ve'), ('six', 'si6ix'), ('seven', 'se7ven'), ('eight', 'ei8ght'), ('nine', 'ni9ne')]
+import re
+pairs = [('one', 'on1ne'), ('two', 'tw2wo'), ('three', 'thr3ree'), ('four', 'fo4ur'), ('five', 'fi5ve'), ('six', 'si6ix'), ('seven', 'se7ven'), ('eight', 'ei8ght'), ('nine', 'ni9ne')]
 sum = 0;
-for line in input:
+for line in open("1.in"):
     _ = [0 for w,d in pairs if (line := line.replace(w,d))]
-    l = [int(char) for char in line.rstrip() if ord(char) < 58]
-    print(sum := sum + (l[0]*10 + l[-1]))
+    print(sum := sum + (int(re.search(r'\d', line.rstrip()).group(0))*10 + int(re.search(r'\d', line.rstrip()[::-1]).group(0))))
