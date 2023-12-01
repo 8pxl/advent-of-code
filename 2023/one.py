a92ea50a2beb4ce6d876f04dc92f5ec558764afe
8pxl <56054380+8pxl@users.noreply.github.com>
2023-11-30 22:54:02 -0800

    7 lines

---
+++ 2023/one.py
@@ -0,0 +1,7 @@
+input = open("1.in")
+pairs = [ ('one', 'on1ne'), ('two', 'tw2wo'), ('three', 'thr3ree'), ('four', 'fo4ur'), ('five', 'fi5ve'), ('six', 'si6ix'), ('seven', 'se7ven'), ('eight', 'ei8ght'), ('nine', 'ni9ne')]
+sum = 0;
+for line in input:
+    _ = [0 for w,d in pairs if (line := line.replace(w,d))]
+    l = [int(char) for char in line.rstrip() if ord(char) < 58]
+    print(sum := (l[0]*10 + l[-1]))
