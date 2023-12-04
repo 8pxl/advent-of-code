6ca6907a21eff0b77693291458e07a1a1cdb4a98
8pxl <56054380+8pxl@users.noreply.github.com>
2023-12-03 22:25:15 -0800

    clean up

--- 2023/4/4.2.py
+++ 2023/4/4.2.py
@@ -2,7 +2,6 @@ cards = [0] * 206
 for i,line in enumerate(open("4/4.in")):
     cards[i] += 1
     l = line.split()[2:]
-    c = [list(map(int, l[:10])), list(map(int, l[11:]))]
-    for j in range(i+1, i + 1 + len([1 for num in c[1] if num in c[0]])):
+    for j in range(i+1, i + 1 + len([1 for num in l[11:] if num in l[:10]])):
         cards[j] += (1 * cards[i])
 print(sum(cards))
--- 2023/4/4.py
+++ 2023/4/4.py
@@ -1,7 +1,6 @@
 total = 0
 for line in open("4/4.in"):
     l = line.split()[2:]
-    c = [list(map(int, l[:10])), list(map(int, l[10+1:]))]
-    num = len([1 for num in c[1] if num in c[0]]) - 1
+    num = len([1 for num in l[11:] if num in l[:10]]) - 1
     if (num>=0):
         print(total := total + (1 << num))
