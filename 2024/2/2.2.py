c3e0acf85522c6a8a81a0602588c187d32eb978d
8pxl <56054380+8pxl@users.noreply.github.com>
2024-12-02 12:25:14 -0800

    change mono check

--- 2024/2/2.2.py
+++ 2024/2/2.2.py
@@ -1,35 +1,17 @@
 f = open("2/2.in")
-count = 0
-
-
-def strictly_increasing(L):
-    return all(L[i] < L[i+1] for i in range(len(L)-1))
-
-
-def strictly_decreasing(L):
-    return all(L[i] > L[i+1] for i in range(len(L)-1))
-
-
-def monotonic(L):
-    return strictly_increasing(L) or strictly_decreasing(L)
 
 def check(nums):
-    q = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
-    n = [1 <= abs(num) <= 3 for num in q]
-    return ((n.count(True) == len(n)) and monotonic(nums))
+    diff = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
+    n = [1 <= abs(num) <= 3 for num in diff]
+    monotonic = (all(x > 0 for x in diff) or all(x < 0 for x in diff))
+    return (n.count(True) == len(n)) and monotonic
 
+count = 0
 for line in f.readlines():
     nums = tuple(map(int, line.split()))
     safe = check(nums)
-    # print(safe)
-    if not safe:
-        for i in range(len(nums)):
-            if (not safe):
-                print(f"num to check: {(nums[:i] + nums[i+1:])}")
-                print(f"value: {check(nums[:i] + nums[i+1:])}")
-                safe = check(nums[:i] + nums[i+1:])
-            else:
-                # print(line.rstrip() + " " + str(safe))
-                break
+    for i in range(len(nums)):
+        if (not safe):
+            safe = check(nums[:i] + nums[i+1:])
     count += safe
 print(count)
--- 2024/2/2.py
+++ 2024/2/2.py
@@ -1,22 +1,9 @@
 f = open("2/2.in")
 count = 0
-
-
-def strictly_increasing(L):
-    return all(L[i] < L[i+1] for i in range(len(L)-1))
-
-
-def strictly_decreasing(L):
-    return all(L[i] > L[i+1] for i in range(len(L)-1))
-
-
-def monotonic(L):
-    return strictly_increasing(L) or strictly_decreasing(L)
-
-
 for line in f.readlines():
     nums = tuple(map(int, line.split()))
-    q = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
-    n = [1 <= abs(num) <= 3 for num in q]
-    count += (n.count(n[0]) == len(n) and monotonic(nums))
+    diff = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
+    n = [1 <= abs(num) <= 3 for num in diff]
+    monotonic = (all(x > 0 for x in diff) or all(x < 0 for x in diff))
+    count += (n.count(True) == len(n) and monotonic)
 print(count)
