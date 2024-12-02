f20f3a417719871701d71e550914b2f8b1d6cf84
8pxl <56054380+8pxl@users.noreply.github.com>
2024-12-02 12:38:59 -0800

    combine parts

--- 2024/2/2.py
+++ 2024/2/2.py
@@ -1,9 +1,9 @@
-f = open("2/2.in")
 count = 0
-for line in f.readlines():
-    nums = tuple(map(int, line.split()))
-    diff = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
-    n = [1 <= abs(num) <= 3 for num in diff]
-    monotonic = (all(x > 0 for x in diff) or all(x < 0 for x in diff))
-    count += (n.count(True) == len(n) and monotonic)
+with open("2/2.in") as f:
+    for line in f.readlines():
+        nums = tuple(map(int, line.split()))
+        diff = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
+        n = [1 <= abs(num) <= 3 for num in diff]
+        monotonic = (all(x > 0 for x in diff) or all(x < 0 for x in diff))
+        count += (n.count(True) == len(n) and monotonic)
 print(count)
