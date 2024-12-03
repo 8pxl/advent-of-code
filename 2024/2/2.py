3d526ba7c2e2a05acd4a675ea682693977a2dd24
8pxl <56054380+8pxl@users.noreply.github.com>
2024-12-02 16:11:22 -0800

    Update 2.py

--- 2024/2/2.py
+++ 2024/2/2.py
@@ -1,9 +1,12 @@
-count = 0
+def check(nums):
+    diff = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
+    monotonic = (all(x > 0 for x in diff) or all(x < 0 for x in diff))
+    return all([1 <= abs(num) <= 3 for num in diff]) and monotonic
+
+p1, p2 = 0, 0
 with open("2/2.in") as f:
     for line in f.readlines():
         nums = tuple(map(int, line.split()))
-        diff = [nums[i+1] - nums[i] for i in range(len(nums)-1)]
-        n = [1 <= abs(num) <= 3 for num in diff]
-        monotonic = (all(x > 0 for x in diff) or all(x < 0 for x in diff))
-        count += (n.count(True) == len(n) and monotonic)
-print(count)
+        p1 += check(nums)
+        p2 += sum([check(nums[:i] + nums[i+1:]) for i in range(len(nums))]) > 0
+print(p1, p2)
