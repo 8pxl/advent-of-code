94edb1e4ceb1edcd577e6b89b58890fd426450db
8pxl <56054380+8pxl@users.noreply.github.com>
2024-12-07 01:08:52 -0800

    clean

--- 2024/7/7.py
+++ 2024/7/7.py
@@ -1,13 +1,8 @@
 def check(num, const):
     print(num, const)
-    if (len(const) == 1):
-        if (num == const[0]):
-            return True
-        else:
-            return False
+    if (len(const) == 1): return (num == const[0])
     if (num % const[-1] == 0):
-        if check(num / const[-1], const[:-1]):
-            return True
+        if check(num / const[-1], const[:-1]): return True
     return check(num - const[-1], const[:-1])
 sum = 0
 for line in open("7/7.in"):
