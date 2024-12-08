0de39f8394bb6792a1046618506a14adce82e603
8pxl <56054380+8pxl@users.noreply.github.com>
2024-12-07 22:18:52 -0800

    make concise

--- 2024/8/8.2.py
+++ 2024/8/8.2.py
@@ -1,15 +1,9 @@
-map = {}
-dim = [0,0]
+map, dim, pts = {}, [], set()
 for i, line in enumerate(open("8/8.in")):
-    dim[0] = len(line.rstrip())
-    dim[1] = i+1
+    dim = [len(line.rstrip()), i+1]
     for j, char in enumerate(line.rstrip()):
         if char != '.':
-            if char not in map.keys():
-                map[char] = [complex(j, i)]
-            else:
-                map[char].append(complex(j, i))
-pts = set()
+            map.setdefault(char, []).append(complex(j, i))
 for node in map.keys():
     for k in map[node]:
         for other in map[node]:
@@ -21,8 +15,7 @@ for node in map.keys():
                 if (0 <= cand.real < dim[0]) and (0 <= cand.imag < dim[1]):
                     pts.add(cand)
                 else:
-                    if (mult < 0):
-                        break
+                    if (mult < 0): break
                     mult = 0
-                mult+= 1 if mult > 0 else -1
+                mult += 1 if mult > 0 else -1
 print(len(pts))
--- 2024/8/8.py
+++ 2024/8/8.py
@@ -1,20 +1,13 @@
-map = {}
-dim = [0,0]
+map, dim, pts = {}, [], set()
 for i, line in enumerate(open("8/8.in")):
-    dim[0] = len(line.rstrip())
-    dim[1] = i+1
+    dim = [len(line.rstrip()), i+1]
     for j, char in enumerate(line.rstrip()):
         if char != '.':
-            if char not in map.keys():
-                map[char] = [complex(j, i)]
-            else:
-                map[char].append(complex(j, i))
-pts = set()
+            map.setdefault(char, []).append(complex(j, i))
 for node in map.keys():
     for k in map[node]:
         for other in map[node]:
-            if k == other:
-                continue
+            if k == other: continue
             cand = k - (other - k)
             if (0 <= cand.real < dim[0]) and (0 <= cand.imag < dim[1]):
                 pts.add(cand)
